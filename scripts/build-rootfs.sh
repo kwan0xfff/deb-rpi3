25e1ae01638bd4bb6c7ed4443c4a6e4383982fd3
R. J. Dev. Kwan <kwan0xfff@gmail.com>
2017-11-12 21:46:39 -0800

    build-rootfs -- first cut for calling from parent.

--- scripts/build-rootfs.sh
+++ scripts/build-rootfs.sh
@@ -1,47 +1,79 @@
 #!/bin/sh
-
-#
 # build-rootfs.sh
-#
-# Construct Debian-based root filesystem image for Raspberry Pi 3
+
+# Construct Debian-based root filesystem image for
+# Raspberry Pi 3.
 # Currently, only executes debootstrap stage 1
 
 # basic values
-RTFS_RELEASE=${RTFS_RELEASE=stretch}
-RTFS_ARCH=${RTFS_ARCH=arm64}
-KRNL_ARCH=${KRNL_ARCH=aarch64}
-REPO_HOST=http://ftp.us.debian.org/debian
+RELEASE=${RELEASE=stretch}
+ARCH=${ARCH=arm64}
+REPO=http://ftp.us.debian.org/debian
 
 # packages
-PKG_INCL=build-essential,gcc-6,gdb,e17,wicd
+PKGINCL=build-essential,gcc-6,gdb,e17,wicd
 
-# derived values
-CROSSGCC=${KRNL_ARCH}-linux-gnu-gcc
-CROSSPATH=/opt/linaro/gcc-linaro-aarch64
-PATH=${CROSSPATH}/bin:${PATH}
+# CROSSPATH=/opt/linaro/gcc-linaro-aarch64
+# PATH=${CROSSPATH}/bin:${PATH}
 
 # 'build' directory tree
-BLDDIR=${BLDDIR=$(pwd)/build}
+do_rootfs() {
+    # Do debootstrap as root
+    (   cd ${RTRG}
+        echo debootstrap --arch=${ARCH} \
+            --include=${PKGINCL} --foreign ${RTFS_RELEASE} \
+            $(pwd) ${REPO}
+    )
+}
 
-if [ ! -d "${BLDDIR}" ]; then
-    mkdir ${BLDDIR}
-    mkdir ${BLDDIR}/kernel
-    mkdir ${BLDDIR}/rootfs
-fi
-
-# check compiler
-${CROSSGCC} --version 2>&1 > /dev/null
-#echo got $?
-
-if [ $? = 0 ]; then
-    echo ${CROSSGCC} found in PATH
-else
-    echo cannot find ${CROSSGCC} in PATH
+# want enhanced getopt
+getopt --test > /dev/null 
+if [ $? -ne 4 ] ; then
+    echo "sorry, getopt is incorrect version."
     exit 1
 fi
 
-# Do debootstrap as root
-cd ${BLDDIR}/rootfs
-debootstrap --arch=${RTFS_ARCH} --include=${PKG_INCL} --foreign ${RTFS_RELEASE} \
-    $(pwd) ${REPO_HOST}
+OPTIONS=a:p:i:r:R:t:v
+LONGOPTIONS=arch:,tcpfx:,pkgincl:,release:,tcroot:,verbose
+
+PARSED=$(getopt -o $OPTIONS -l $LONGOPTIONS --name "$0" -- "$@")
+
+eval set -- "${PARSED}"
+
+while true; do
+    case "$1" in
+        -a|--arch) ARCH=$2 ; shift 2 ;;
+        -p|--tcpfx) TCPFX=$2 ; shift 2 ;;
+        -i|--pkgincl) PKGINCL=$2 ; shift 2 ;;
+        -r|--repo) REPO=$2 ; shift 2 ;;
+        -R|--release) RELEASE=$2 ; shift 2 ;;
+        -t|--tcroot) TCROOT=$2 ; shift 2 ;;
+        -v|--verbose) VERBOSE=true ; shift ;;
+        --) shift ; break ;;
+        *) echo "Internal error\!"; exit 1 ;;
+    esac
+done
+
+echo remaining:  $*
+
+# Process positional arguments
+
+subcmd=$1
+shift
+
+echo subcommand: ${subcmd}
+echo remaining:  $*
+
+case ${subcmd} in
+  rootfs)
+    RTRG=$1 ; shift
+    do_rootfs
+    ;;
+  *)
+    echo "Unknown subcommand:" ${subcmd}
+    exit 1
+    ;;
+esac
+
+exit 0
 
